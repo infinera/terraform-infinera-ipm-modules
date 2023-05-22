a4d6386a7ea820118ecd03df3e8f0d22dbf1707e
Lam Hoang <lhoang@infinera.com>
2023-05-22 10:47:24 -0700

    updated

--- use-cases/env.sh
+++ use-cases/env.sh
@@ -1,4 +1,4 @@
-export TF_VAR_ipm_user="xr-user-1"
-export TF_VAR_ipm_host="https://pt-xrivk824-dv"
-export TF_VAR_ipm_password="infinera"
-export TF_VAR_ipm_profile_path="../ipm-profiles"
+export TF_VAR_ipm_user=""
+export TF_VAR_ipm_host=""
+export TF_VAR_ipm_password=""
+export TF_VAR_ipm_profile_path=""
